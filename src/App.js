d33026bc519788ae54e205a89baa48c93c25a27a
sreedevi26 <sreedevimangalath26@gmail.com>
2023-10-26 15:21:36 +0530

    Background color changer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { SketchPicker } from 'react-color';
 
 function App() {
+  const [colors,setColor] = useState("#ff6")
+  const handleChange =(color)=>{
+    setColor(color.hex)
+    // console.log(color);
+  }
+
+  const styles = {
+    backgroundColor:colors,
+    height:'100vh',
+    textAlign:'center'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles}>
+      <h1 className='align-items-center'>Color Picker</h1>
+      <SketchPicker className='justify-content-center align-item-center flex-column'
+      color={colors}
+      onChangeComplete={handleChange}/>
     </div>
   );
 }
